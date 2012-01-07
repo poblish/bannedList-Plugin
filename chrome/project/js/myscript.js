3bffa4586106b10756add3290b3eedc004ea465d
Andrew Regan <aregan@gmail.com>
2012-01-07 14:52:19 +0000

    Initial commits
    Version 0.0.1 - doesn't work

---
+++ chrome/project/js/myscript.js
@@ -0,0 +1,21 @@
+function getTextNodesIn(node, includeWhitespaceNodes) {
+console.log( node );
+    var textNodes = [], whitespace = /^\s*$/;
+
+    function getTextNodes(node) {
+        if (node.nodeType == 3) {
+            if (includeWhitespaceNodes || !whitespace.test(node.nodeValue)) {
+                textNodes.push(node);
+            }
+        } else if ( node.childNodes != null) {
+            for (var i = 0, len = node.childNodes.length; i < len; ++i) {
+                getTextNodes(node.childNodes[i]);
+            }
+        }
+    }
+
+    getTextNodes(node);
+    return textNodes;
+}
+
+getTextNodesIn(  $('body')[0] );
