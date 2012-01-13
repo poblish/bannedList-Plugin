d30fa5f3abfcda0dd4ed1a2635230d24e81561a9
Andrew Regan <aregan@gmail.com>
2012-01-13 00:34:29 +0000

    Initial working Firefox plugin
    Simplest possible project able to display matches.

---
+++ firefox/project/lib/main.js
@@ -0,0 +1,31 @@
+const widgets = require("widget");
+
+var data = require('self').data;
+// const tabs = require("tabs");
+
+
+var widget = widgets.Widget({
+  id: "bannedListLink",
+  label: "#BannedList Highlighter",
+  contentURL: data.url("img/icon16.png") /* ,
+  onClick: function() {
+    tabs.open("http://www.mozilla.org/");
+  } */
+});
+
+var pageMod = require("page-mod");
+pageMod.PageMod({
+  include: ['*'],
+  contentScriptWhen: "end",
+  contentScriptFile: [ data.url("js/jquery-1.7.1.min_plus_highlight.js"), data.url("js/bannedList.js") ],
+/*
+  onAttach: function onAttach(worker) {
+    console.log("Attaching content scripts")
+    worker.on('message', function(data) {
+      console.log(data);
+    });
+  }
+*/
+});
+
+console.log("The add-on is running.");
