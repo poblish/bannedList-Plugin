c32980114b9004b666848b1dd105dec92603d268
Andrew Regan <aregan@gmail.com>
2012-04-29 15:12:12 +0100

    New terms; don't submit stats for Amazon pages

--- chrome/project/js/contentSupport.js
+++ chrome/project/js/contentSupport.js
@@ -150,7 +150,7 @@ function getContentStatsWhiteListFor( inDocURL ) {
 }
 
 function getIgnoreStatsPageFilterRegex() {
-    return /http.*(appengine.google.com*)|(bannedlist-stats*)|(poblish.org\/downloads\/TheList.html)|.github.?|.google.?|.ebay.?|.ebay.?|.bing.?|.facebook.?/;
+    return /http.*(appengine.google.com*)|(bannedlist-stats*)|(poblish.org\/downloads\/TheList.html)|.github.?|.google.?|.amazon.?|.ebay.?|.bing.?|.facebook.?/;
 }
 
 function trimUrlForStats( inURL ) {
