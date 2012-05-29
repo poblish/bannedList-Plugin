3153d3a4d0683f4c32cf263d77f63009c0edee4c
Andrew Regan <aregan@gmail.com>
2012-05-29 19:46:18 +0100

    Exclude more WordPress bumph

--- firefox/project/data/js/contentSupport.js
+++ firefox/project/data/js/contentSupport.js
@@ -155,7 +155,7 @@ function getContentStatsBlackListFor( inDocURL ) {
         return $('div.beta, div#js-comments');
     }
 
-    return $('div#comments, div#allcomments, div#disqus_thread, div#most-popular, div#sidebar, div#sidebar-right-1, div#single-rightcolumn, div#sidebar-first, div#all-comments, div#discussion-comments, div#reader-comments, div#beta, div#bottom, div#departments, div#promo, div#secondaryColumn, section#comments-area');
+    return $('div#comments, div#allcomments, div#disqus_thread, div#most-popular, div#sidebar, div#sidebar-right-1, div#single-rightcolumn, div#sidebar-first, div#all-comments, div#discussion-comments, div#reader-comments, div#beta, div#bottom, div#departments, div#promo, div#secondaryColumn, ul#menus, li.cat-item, section#comments-area');
 }
 
 function getContentStatsWhiteListFor( inDocURL ) {
